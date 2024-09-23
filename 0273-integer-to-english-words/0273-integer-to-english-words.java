63a882dc28d4aae57c6ac02572d6f65739fd7079
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-23 11:51:58 +0530

    Time: 1 ms (100%), Space: 41.4 MB (97.06%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,22 +1,29 @@
-public class Solution {
-    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    
-    public String numberToWords(int num) {
-        if (num == 0) return "Zero";
-        return helper(num); 
+class Solution {
+    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int n) {
+        if(n == 0) {
+            return "Zero";
+        }
+        return num(n).trim();
     }
-    
-    private String helper(int num) {
-        String result = new String();
-        if (num < 10) result = belowTen[num];
-        else if (num < 20) result = belowTwenty[num -10];
-        else if (num < 100) result = belowHundred[num/10] + " " + helper(num % 10);
-        else if (num < 1000) result = helper(num/100) + " Hundred " +  helper(num % 100);
-        else if (num < 1000000) result = helper(num/1000) + " Thousand " +  helper(num % 1000);
-        else if (num < 1000000000) result = helper(num/1000000) + " Million " +  helper(num % 1000000);
-        else result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
-        return result.trim();
+    String num(int n) {
+        if(n < 20) {
+            return ones[n];
+        }
+        if(n < 100) {
+            return new StringBuilder().append(tens[n / 10]).append(' ').append(ones[n % 10]).toString();
+        }
+        if(n < 1000) {//123
+            return new StringBuilder().append(ones[n / 100]).append(" Hundred ").append(num(n % 100)).toString();
+        }
+        if(n < 1000_000) {
+            return new StringBuilder().append(num(n / 1000).trim()).append(" Thousand ").append(num(n % 1000)).toString();
+        }
+        if(n < 1000_000_000) {
+            return new StringBuilder().append(num(n / 1000_000).trim()).append(" Million ").append(num(n % 1000_000)).toString();
+        }
+        return new StringBuilder().append(num(n / 1000_000_000).trim()).append(" Billion ").append(num(n % 1000_000_000)).toString();
     }
 }
