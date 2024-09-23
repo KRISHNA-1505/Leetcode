3e34c20265a141ebf7ed2b76c65103ec8a59ed20
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-23 09:44:07 +0530

    Time: 1516 ms (7.91%), Space: 55.5 MB (72.17%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[] nums;
+    Random random;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.random = new Random();
+    }
+    public int pick(int target) {
+        int idx = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                count++;
+                if (random.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
+    }
+}
