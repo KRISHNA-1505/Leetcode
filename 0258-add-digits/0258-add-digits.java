2dd3cb4f106f8bacc0e9efbef89dd6da4d8ea5e2
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-23 09:17:54 +0530

    Time: 1 ms (52.32%), Space: 40.8 MB (44.42%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,11 @@
 class Solution {
     public int addDigits(int num) {
-        int rem=0,rev=0,s=0;
+        int rem=0,rev=0,s=10;
         if(num<10)
         {
             return num;
         }
-         while(num!=0)
-            {
-                rem=num%10;
-                s+=rem;
-                num/=10;
-            }
-            num=s;
+       
         while(s>=10)
         {
             s=0;
