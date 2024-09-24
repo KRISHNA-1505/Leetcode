fe0cbe40b389bfbbc8255ae11b0511634f1af792
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-24 13:35:09 +0530

    Time: 0 ms (100%), Space: 45.5 MB (21.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,7 +4,7 @@ class Solution {
         int sum =0;
         
         for(int i=0;i<nums.length;i++){ 
-            sum=+sum+nums[i];
+            sum+=nums[i];
         }
         int  totalNum=nums.length*(nums.length+1)/2;
         int misNum=totalNum-sum;
