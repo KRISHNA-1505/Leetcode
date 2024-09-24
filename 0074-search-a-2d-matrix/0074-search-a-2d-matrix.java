0ecaa375f0ad268bedd6b5ca717312cf343fe155
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-24 11:08:35 +0530

    Time: 0 ms (100%), Space: 42 MB (52.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,15 +3,9 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
-            }
-        }
+        for(int j=0;j<m;j++)
+        if(matrix[i][j]==target)
+        return true;
         return false;
     }
 }
