1e5dc5d399dab779a0eec53c8c7751c12df1d027
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-09-30 15:38:26 +0530

    Time: 83 ms (50%), Space: 56.9 MB (20.49%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,8 +1,6 @@
 class Solution {
-
     private Map<Integer, List<Integer>> map;
     private Random random;
-
     public Solution(int[] nums) {
         map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -16,10 +14,8 @@ class Solution {
         }
         random = new Random();
     }
-
     public int pick(int target) {
         List<Integer> indexes = map.get(target);
         return indexes.get(random.nextInt(indexes.size()));
     }
-
 }
