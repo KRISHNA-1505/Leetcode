42119d31c7c43527b891a6c9ecd06ec843966a85
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-10-05 19:32:04 +0530

    Time: 153 ms (72.93%), Space: 44.5 MB (75.62%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.java
+++ 0464-can-i-win/0464-can-i-win.java
@@ -1,42 +1,26 @@
 class Solution {
-    Map<Integer,Boolean>map;
-    boolean[] used;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-       int sum=(1+maxChoosableInteger)*maxChoosableInteger/2;
-       if(sum<desiredTotal)return false;
-       if(desiredTotal<=0)return true;
-       map=new HashMap();
-       used=new boolean[maxChoosableInteger+1];
-       return helper(desiredTotal);
+        int maxsum = (maxChoosableInteger*(maxChoosableInteger+1))/2;
+        if(maxsum < desiredTotal)
+            return false;
 
+        byte[] memo = new byte[1 << maxChoosableInteger];
+        return helper(0, maxChoosableInteger, 0, desiredTotal, memo);
     }
-    public boolean helper(int desiredTotal){
-        if(desiredTotal<=0)return false;
-        int key=format(used);
-        if(!map.containsKey(key)){
-            for(int i=1;i<used.length;i++)
-            {
-                if(!used[i]){
-                    used[i]=true;
-                    if(!helper(desiredTotal-i)){
-                        map.put(key,true);
-                        used[i]=false;
-                        return true;
-                    }
-                    used[i]=false;
+    private boolean helper(int nums, int len,  int curr, int total, byte[] memo) {
+        if(memo[nums] != 0) {
+           return memo[nums] == 1;
+        }
+        for(int i = 0; i < len; i++) {
+            if((nums & (1 << i)) == 0) {
+                if(curr + i +1 >= total || !helper((nums| (1<<i)), len, 
+                 curr + i +1, total, memo)) {
+                    memo[nums] = 1;
+                    return true;
                 }
             }
-            map.put(key,false);
         }
-        return map.get(key);
-    }
-    public int format(boolean[] used)
-    {
-        int num=0;
-        for(boolean b:used){
-            num<<=1;
-            if(b) num|=1;
-        }
-        return num;
-    }
+        memo[nums] =2;
+        return false;
+    } 
 }
