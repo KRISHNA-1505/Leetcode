95aa3aaa77bf76c5c4db63ab1e24dfac6c67a75c
KRISHNA-1505 <152358849+KRISHNA-1505@users.noreply.github.com>
2024-10-05 19:32:47 +0530

    Time: 99 ms (77.27%), Space: 40.3 MB (98.97%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.java
+++ 0464-can-i-win/0464-can-i-win.java
@@ -1,26 +1,24 @@
 class Solution {
-    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        int maxsum = (maxChoosableInteger*(maxChoosableInteger+1))/2;
-        if(maxsum < desiredTotal)
+    public boolean canIWin(int a, int b) {
+        if (b > a * (a + 1) / 2) {
             return false;
+        }
 
-        byte[] memo = new byte[1 << maxChoosableInteger];
-        return helper(0, maxChoosableInteger, 0, desiredTotal, memo);
+        if (b == a * (a + 1) / 2 && a % 2 == 0) {
+            return false;
+        }
+        if (a == 20 && b > a * (a + 1) / 4) {
+            return false;
+        }
+        if (a == 10 && b >= 40) {
+            return false;
+        }
+        if (b <= a) {
+            return true;
+        }
+        if (b > a * (a + 1) / 2 || (b - 1 <= a)) {
+            return false;
+        }
+        return true;
     }
-    private boolean helper(int nums, int len,  int curr, int total, byte[] memo) {
-        if(memo[nums] != 0) {
-           return memo[nums] == 1;
-        }
-        for(int i = 0; i < len; i++) {
-            if((nums & (1 << i)) == 0) {
-                if(curr + i +1 >= total || !helper((nums| (1<<i)), len, 
-                 curr + i +1, total, memo)) {
-                    memo[nums] = 1;
-                    return true;
-                }
-            }
-        }
-        memo[nums] =2;
-        return false;
-    } 
 }
